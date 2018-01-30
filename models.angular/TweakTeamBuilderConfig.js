e2803b8457119b3859b8d894c2da66f1cb21cd83
Matteo Zambon <matteo@harpoonconnect.com>
2018-01-30 14:57:48 -0200

    Updated to the latest development

--- models.angular/TweakTeamBuilderConfig.js
+++ models.angular/TweakTeamBuilderConfig.js
@@ -13,11 +13,13 @@
          * @param debugMode               {boolean}   default: false  
          * @param features                {object}    default:   
          * @param ui                      {object}    default:   
+         * @param isDefault               {boolean}   default: false  
          * @param created                 {string}    format: date-time  
          * @param modified                {string}    format: date-time  
          * @param id                      {string}    
          * @param teamId                  {string}    
          * @param team                    {object}    $ref: #/definitions/Team  
+         * @param portals                 {array}     items: $ref: #/definitions/Portal    
          * @param productGroups           {array}     items: $ref: #/definitions/ProductGroup    
          * @param productTypes            {array}     items: $ref: #/definitions/ProductType    
          * @param productSizes            {array}     items: $ref: #/definitions/ProductSize    
@@ -49,8 +51,8 @@
         /**
          * Private properties
          */
-        var parameters = ['name', 'debugMode', 'features', 'ui', 'created', 'modified', 'id', 'teamId', 'team', 'productGroups', 'productTypes', 'productSizes', 'productSizeMaterials', 'productSizeMaterialsRel'];
-        var parametersType = ['string', 'boolean', 'object', 'object', 'string', 'string', 'string', 'string', 'object', 'array', 'array', 'array', 'array', 'array'];
+        var parameters = ['name', 'debugMode', 'features', 'ui', 'isDefault', 'created', 'modified', 'id', 'teamId', 'team', 'portals', 'productGroups', 'productTypes', 'productSizes', 'productSizeMaterials', 'productSizeMaterialsRel'];
+        var parametersType = ['string', 'boolean', 'object', 'object', 'boolean', 'string', 'string', 'string', 'string', 'object', 'array', 'array', 'array', 'array', 'array', 'array'];
         var requiredParameters = [];
 
         /**
