5a27c57f287ae5f1dc7c380061c96ed565911688
Matteo Zambon <matteo@harpoonconnect.com>
2018-02-08 08:47:19 -0200

    Released latest Stage

--- models.angular/TweakBilling.js
+++ models.angular/TweakBilling.js
@@ -13,11 +13,15 @@
          * @param companyEmail         {string}    
          * @param companyVat           {string}    
          * @param companyCard          {object}    $ref: #/definitions/BillingCard  
+         * @param companyBankAccount   {object}    $ref: #/definitions/BillingBankAccount  
+         * @param companySource        {object}    $ref: #/definitions/BillingSource  
          * @param subscription         {object}    $ref: #/definitions/BillingSubscription  
          * @param limit                {object}    $ref: #/definitions/BillingLimit  
          * @param taxPercent           {number}    default: 0  format: double  
          * @param stripeCustomerId     {string}    
          * @param stripeCardId         {string}    
+         * @param stripeBankAccountId  {string}    
+         * @param stripeSourceId       {string}    
          * @param stripeSubscriptionId {string}    
          * @param id                   {string}    
          * @param teamId               {string}    
@@ -48,8 +52,8 @@
         /**
          * Private properties
          */
-        var parameters = ['companyName', 'companyEmail', 'companyVat', 'companyCard', 'subscription', 'limit', 'taxPercent', 'stripeCustomerId', 'stripeCardId', 'stripeSubscriptionId', 'id', 'teamId', 'team'];
-        var parametersType = ['string', 'string', 'string', 'object', 'object', 'object', 'number', 'string', 'string', 'string', 'string', 'string', 'object'];
+        var parameters = ['companyName', 'companyEmail', 'companyVat', 'companyCard', 'companyBankAccount', 'companySource', 'subscription', 'limit', 'taxPercent', 'stripeCustomerId', 'stripeCardId', 'stripeBankAccountId', 'stripeSourceId', 'stripeSubscriptionId', 'id', 'teamId', 'team'];
+        var parametersType = ['string', 'string', 'string', 'object', 'object', 'object', 'object', 'object', 'number', 'string', 'string', 'string', 'string', 'string', 'string', 'string', 'object'];
         var requiredParameters = [];
 
         /**
