a59fc9b799d35b95e0e3891a1cd084b72378d9be
Bluemix Toolchain <matteo@harpoonconnect.com>
2018-07-12 12:26:10 +0000

    Released v1.0.14-alpha.16 #deploy

---
+++ models.angular/TweakAssetCategoryPermissionSet.js
@@ -0,0 +1,95 @@
+(function () {
+    'use strict';
+    angular.module('Tweak')
+        .factory('TweakAssetCategoryPermissionSet', TweakAssetCategoryPermissionSet);
+
+    TweakAssetCategoryPermissionSet.$inject = ['$log'];
+
+    function TweakAssetCategoryPermissionSet($log) {
+
+        /**
+         * Constructor, with class name
+         * @param assetCategoryId                {string}    [REQUIRED]    
+         * @param value                          {object}                  
+         * @param selectType                     {string}                  enum: autoComplete, text, longText, date, linkAsset, sourcefile, vector
+         * @param filterable                     {boolean}                 default: true  
+         * @param required                       {boolean}                 default: true  
+         * @param mainFilter                     {boolean}                 default: true  
+         * @param multiSelect                    {boolean}                 default: true  
+         * @param multiFilter                    {boolean}                 default: true  
+         * @param displayField                   {boolean}                 default: true  
+         * @param showInListView                 {boolean}                 default: true  
+         * @param showInDuplicateView            {boolean}                 default: true  
+         * @param showInGridView                 {boolean}                 default: true  
+         * @param showInContactSheetView         {boolean}                 default: true  
+         * @param hidePublic                     {boolean}                 default: true  
+         * @param hideOptionsWithoutDependencies {boolean}                 default: true  
+         * @param id                             {string}                  
+         */
+        function TweakAssetCategoryPermissionSet(data) {
+            data = data || {};
+
+            for (var d in data) {
+                this[d] = data[d];
+            }
+
+
+            for (var i = 0; i < parameters.length; i++) {
+
+                if (this[parameters[i]] && parametersType[i] === 'string' ) {
+
+                    this[parameters[i]] = '' + this[parameters[i]];
+
+                }
+
+            }
+
+
+            constructorValidation(this);
+        }
+
+        /**
+         * Private properties
+         */
+        var parameters = ['assetCategoryId', 'value', 'selectType', 'filterable', 'required', 'mainFilter', 'multiSelect', 'multiFilter', 'displayField', 'showInListView', 'showInDuplicateView', 'showInGridView', 'showInContactSheetView', 'hidePublic', 'hideOptionsWithoutDependencies', 'id'];
+        var parametersType = ['string', 'object', 'string', 'boolean', 'boolean', 'boolean', 'boolean', 'boolean', 'boolean', 'boolean', 'boolean', 'boolean', 'boolean', 'boolean', 'boolean', 'string'];
+        var requiredParameters = ['assetCategoryId'];
+
+        /**
+         * Private function
+         */
+        function constructorValidation(model) {
+            requiredParameters.forEach(function(requiredParameter) {
+                if (model[requiredParameter] === undefined) {
+                    throw new Error('Required parameter `' + requiredParameter + '` is missing!');
+                }
+            });
+
+            for (var i = 0; i < parameters.length; i++) {
+                var parameterTypeObject = '[object ' + parametersType[i].charAt(0).toUpperCase() + parametersType[i].substr(1) + ']';
+                if (model[parameters[i]] && Object.prototype.toString.call(model[parameters[i]]) !== parameterTypeObject) {
+                    throw new Error('Wrong parameter type for `' + parameters[i] + '`: should be `' + parametersType[i] + '`!');
+                }
+            }
+        }
+
+        /**
+         * Static method, assigned to class
+         */
+        TweakAssetCategoryPermissionSet.build = function (data) {
+            return new TweakAssetCategoryPermissionSet(data);
+        };
+
+        TweakAssetCategoryPermissionSet.apiResponseTransformer = function (responseData) {
+            if (angular.isArray(responseData)) {
+                return responseData.map(TweakAssetCategoryPermissionSet.build).filter(Boolean);
+            }
+            return TweakAssetCategoryPermissionSet.build(responseData);
+        };
+
+        /**
+         * Return the constructor function
+         */
+        return TweakAssetCategoryPermissionSet;
+    }
+})();
---
+++ models.angular/TweakDimensions.js
@@ -0,0 +1,83 @@
+(function () {
+    'use strict';
+    angular.module('Tweak')
+        .factory('TweakDimensions', TweakDimensions);
+
+    TweakDimensions.$inject = ['$log'];
+
+    function TweakDimensions($log) {
+
+        /**
+         * Constructor, with class name
+         * @param width  {number}    [REQUIRED]    minimum: 0  format: double  
+         * @param height {number}    [REQUIRED]    minimum: 0  format: double  
+         * @param depth  {number}                  minimum: 0  format: double  
+         * @param id     {string}                  
+         */
+        function TweakDimensions(data) {
+            data = data || {};
+
+            for (var d in data) {
+                this[d] = data[d];
+            }
+
+
+            for (var i = 0; i < parameters.length; i++) {
+
+                if (this[parameters[i]] && parametersType[i] === 'string' ) {
+
+                    this[parameters[i]] = '' + this[parameters[i]];
+
+                }
+
+            }
+
+
+            constructorValidation(this);
+        }
+
+        /**
+         * Private properties
+         */
+        var parameters = ['width', 'height', 'depth', 'id'];
+        var parametersType = ['number', 'number', 'number', 'string'];
+        var requiredParameters = ['width', 'height'];
+
+        /**
+         * Private function
+         */
+        function constructorValidation(model) {
+            requiredParameters.forEach(function(requiredParameter) {
+                if (model[requiredParameter] === undefined) {
+                    throw new Error('Required parameter `' + requiredParameter + '` is missing!');
+                }
+            });
+
+            for (var i = 0; i < parameters.length; i++) {
+                var parameterTypeObject = '[object ' + parametersType[i].charAt(0).toUpperCase() + parametersType[i].substr(1) + ']';
+                if (model[parameters[i]] && Object.prototype.toString.call(model[parameters[i]]) !== parameterTypeObject) {
+                    throw new Error('Wrong parameter type for `' + parameters[i] + '`: should be `' + parametersType[i] + '`!');
+                }
+            }
+        }
+
+        /**
+         * Static method, assigned to class
+         */
+        TweakDimensions.build = function (data) {
+            return new TweakDimensions(data);
+        };
+
+        TweakDimensions.apiResponseTransformer = function (responseData) {
+            if (angular.isArray(responseData)) {
+                return responseData.map(TweakDimensions.build).filter(Boolean);
+            }
+            return TweakDimensions.build(responseData);
+        };
+
+        /**
+         * Return the constructor function
+         */
+        return TweakDimensions;
+    }
+})();
