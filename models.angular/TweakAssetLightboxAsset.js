8e4262c3079945c442141b9181930238d1ed40dd
GitLab Pipeline <matteo@harpoonconnect.com>
2019-06-24 08:23:34 +0000

    Update to support tweak-api@1.1.7 by Pipeline (#67666531) on Job (#238537909) build/build-sdk

---
+++ models.angular/TweakAssetLightboxAsset.js
@@ -0,0 +1,87 @@
+(function () {
+    'use strict';
+    angular.module('Tweak')
+        .factory('TweakAssetLightboxAsset', TweakAssetLightboxAsset);
+
+    TweakAssetLightboxAsset.$inject = ['$log'];
+
+    function TweakAssetLightboxAsset($log) {
+
+        /**
+         * Constructor, with class name
+         * @param created         {string}    format: date-time  
+         * @param modified        {string}    format: date-time  
+         * @param id              {object}    $ref: #/definitions/ObjectID  
+         * @param assetId         {object}    $ref: #/definitions/ObjectID  
+         * @param assetLightboxId {object}    $ref: #/definitions/ObjectID  
+         */
+        function TweakAssetLightboxAsset(data) {
+            data = data || {};
+
+            for (var d in data) {
+                this[d] = data[d];
+            }
+
+
+            for (var i = 0; i < parameters.length; i++) {
+
+                if (this[parameters[i]] && parametersType[i] === 'string' ) {
+
+                    this[parameters[i]] = '' + this[parameters[i]];
+
+                }
+
+            }
+
+
+            constructorValidation(this);
+        }
+
+        /**
+         * Private properties
+         */
+        var parameters = ['created', 'modified', 'id', 'assetId', 'assetLightboxId'];
+        var parametersType = ['string', 'string', 'object', 'object', 'object'];
+        var requiredParameters = [];
+
+        /**
+         * Private function
+         */
+        function constructorValidation(model) {
+            requiredParameters.forEach(function(requiredParameter) {
+                if (model[requiredParameter] === undefined) {
+                    throw new Error('Required parameter `' + requiredParameter + '` is missing!');
+                }
+            });
+
+            for (var i = 0; i < parameters.length; i++) {
+                if (parametersType[i].match(/^any$/i)) {
+                    continue;
+                }
+                var parameterTypeObject = '[object ' + parametersType[i].charAt(0).toUpperCase() + parametersType[i].substr(1) + ']';
+                if (model[parameters[i]] && Object.prototype.toString.call(model[parameters[i]]) !== parameterTypeObject) {
+                    throw new Error('Wrong parameter type for `' + parameters[i] + '`: should be `' + parametersType[i] + '`!');
+                }
+            }
+        }
+
+        /**
+         * Static method, assigned to class
+         */
+        TweakAssetLightboxAsset.build = function (data) {
+            return new TweakAssetLightboxAsset(data);
+        };
+
+        TweakAssetLightboxAsset.apiResponseTransformer = function (responseData) {
+            if (angular.isArray(responseData)) {
+                return responseData.map(TweakAssetLightboxAsset.build).filter(Boolean);
+            }
+            return TweakAssetLightboxAsset.build(responseData);
+        };
+
+        /**
+         * Return the constructor function
+         */
+        return TweakAssetLightboxAsset;
+    }
+})();
